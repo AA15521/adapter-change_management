c0f3a49fae42f336569b637dd159e0005a6cb3ec
AA15521 <seethalakshmi.balachandran@centurylink.com>
2020-05-26 12:01:09 +0000

    Lab 5

---
+++ lab04.main.js
@@ -0,0 +1,31 @@
+const options = {
+  url: 'https://dev61798.service-now.com/',
+  username: 'admin',
+  password: 'Yv1zUYrJ3aWr',
+  serviceNowTable: 'change_request'
+};
+
+const path = require('path');
+
+const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
+
+function mainOnObject() {
+  const connector = new ServiceNowConnector(options);
+
+  connector.get((data, error) => {
+                                    if (error) {
+                                        console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
+                                    }
+                                    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
+                                 });
+  
+  connector.post((data, error) => {
+                                    if (error) {
+                                        console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+                                    }
+                                    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
+                                  });
+
+}
+
+mainOnObject();
--- main.js
+++ main.js
@@ -1,31 +1,66 @@
-const options = {
-  url: 'https://dev61798.service-now.com/',
-  username: 'admin',
-  password: 'Yv1zUYrJ3aWr',
-  serviceNowTable: 'change_request'
-};
-
 const path = require('path');
 
 const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
 
-function mainOnObject() {
-  const connector = new ServiceNowConnector(options);
+const EventEmitter = require('events').EventEmitter;
 
-  connector.get((data, error) => {
-                                    if (error) {
-                                        console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-                                    }
-                                    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-                                 });
+class ServiceNowAdapter extends EventEmitter {
+
+  constructor(id, adapterProperties) {
+
+    super();
+
+    this.id = id;
+    this.props = adapterProperties;
+
+    this.connector = new ServiceNowConnector({
+      url: this.props.url,
+      username: this.props.auth.username,
+      password: this.props.auth.password,
+      serviceNowTable: this.props.serviceNowTable
+    });
+  }
   
-  connector.post((data, error) => {
-                                    if (error) {
-                                        console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
-                                    }
-                                    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
-                                  });
+  connect() {
+    this.healthcheck();
+  }
+  
+  healthcheck(callback) {
+    this.emitOnline();
+  }
 
+  emitOffline() {
+    this.emitStatus('OFFLINE');
+    log.warn('ServiceNow: Instance is unavailable.');
+  }
+
+  emitOnline() {
+    this.emitStatus('ONLINE');
+    log.info('ServiceNow: Instance is available.');
+  }
+
+  emitStatus(status) {
+    this.emit(status, { id: this.id });
+  }
+
+  getRecord(callback) {
+      console.log("getRecord is called");
+      log.info('calling getRecord method.');
+  }
+
+  postRecord(callback) {
+      console.log('calling postRecord method.');
+      log.info('calling postRecord method.');
+  }
+  
+  callback(data, error) {
+      if (error) {
+          log.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+          console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+      }
+      log.info(`\nResponse returned from POST request:\n${JSON.stringify(data)}`);
+      console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`);
+  }
 }
 
-mainOnObject();
+module.exports = ServiceNowAdapter;
